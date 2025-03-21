b77c47536235547dfb70d6ad1d62d1ab77f48af4
glaizamaeretardo <retardo.glaizamae18@gmail.com>
2025-03-21 17:19:03 +0800

    Second commit with methods

--- Program.cs
+++ Program.cs
@@ -1,86 +1,104 @@
-﻿//See https:aka.ms/new-console-template for more information
+﻿using System;
 
-using System;
-
-namespace SkincareGuide
+namespace SkinCareGuide
 {
     internal class Program
     {
-        static void Main(string [] args)
+        static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to 5 Steps of Skin Care Routine Guide!");
+            Console.WriteLine("----------------------------------------");
 
-            Console.WriteLine("5 Steps of Skin Care Guide!");
-
-            Console.Write("\nPlease enter your name: ");
+            Console.WriteLine("Please type your name: ");
             string name = Console.ReadLine();
+            Console.WriteLine("----------------------------------------");
 
             while (true)
             {
-                Console.WriteLine("\nPlease choose the number of your skin type:");
-                Console.WriteLine("1 - Oily \n2 - Dry \n3 - Sensitive \n4 - Combination (Oily + Dry)");
-                int st = int.Parse(Console.ReadLine());
+                int skinType = GetUserSkinType();
 
-                Console.WriteLine($"\nHi {name}, based on your skin type, here are your recommended skin care routine:");
-
-                switch (st)
+                if (skinType == -1)
                 {
-                    case 1: //for oily skin
-
-                        Console.WriteLine("- STEP 1: Facial Foaming Cleanser");
-                        Console.WriteLine("- STEP 2: Alcohol-Free Toner");
-                        Console.WriteLine("- STEP 3: Niacinamide Serum");
-                        Console.WriteLine("- STEP 4: Oil-free Moisturizer");
-                        Console.WriteLine("- STEP 5: Matte-Finish Sunscreen");
-
-                        break;
-
-                    case 2: //for dry skin
-
-                        Console.WriteLine("- STEP 1: Hydrating Cream Cleanser");
-                        Console.WriteLine("- STEP 2: Hydrating Toner");
-                        Console.WriteLine("- STEP 3: Hyaluronic Acid Serum");
-                        Console.WriteLine("- STEP 4: Ceramide-rich Moisturizer");
-                        Console.WriteLine("- STEP 5: Moisturizing Sunscreen with SPF 30+");
-
-                        break;
-
-                    case 3: //for sensitive skin
-
-                        Console.WriteLine("- STEP 1: Gentle and Fragrance-free Cleanser");
-                        Console.WriteLine("- STEP 2: Alcohol-Free and Soothing Toner");
-                        Console.WriteLine("- STEP 3: Niacinamide or Centella Asiatica Serum");
-                        Console.WriteLine("- STEP 4: Fragrance-free and Ceramide-rich Moisturizer");
-                        Console.WriteLine("- STEP 5: Mineral (zinc oxide or titanium dioxide) Sunscreen");
-
-                        break;
-
-                    case 4: //for combi skin (oily and dry)
-
-                        Console.WriteLine("- STEP 1: Gel-based or Foaming Cleanser");
-                        Console.WriteLine("- STEP 2: Alcohol-Free and Balancing Toner");
-                        Console.WriteLine("- STEP 3: Niacinamide or Hyaluronic Acid Serum");
-                        Console.WriteLine("- STEP 4: Lightweight and Oil-free Gel Moisturizer");
-                        Console.WriteLine("- STEP 5: Broad-spectrum and Lightweight Sunscreen");
-
-                        break;
-
-                    default:
-
-                        Console.WriteLine("Invalid input! Please enter a number between 1 to 4 only.");
-
-                        break;
+                    Console.WriteLine("----------------------------------------");
+                    Console.WriteLine("Oops! Please type a number between 1 to 4 only.");
+                    continue;
                 }
 
-                Console.WriteLine("\nWould you like another guide? YES or NO?");
-                string response = Console.ReadLine().ToUpper();
+                DisplayFiveSteps(name, skinType);
 
-                if (response != "YES")
+                Console.WriteLine("----------------------------------------");
+                bool continueGuide = AskForAnotherGuide(name);
+
+                if (!continueGuide)
                 {
-                    Console.WriteLine($"\nThank you, {name}! Keep slaying that glow!");
                     break;
                 }
-
             }
+        }
+        static int GetUserSkinType()
+        {
+            Console.WriteLine("Type the number of your skin type: ");
+            Console.WriteLine("1 - Oily \n2 - Dry \n3 - Sensitive \n4 - Combination (Oily + Dry)");
+
+            if (!int.TryParse(Console.ReadLine(), out int skinType) || skinType < 1 || skinType > 4)
+            {
+                return -1;
+            }
+            return skinType;
+        }
+
+        static void DisplayFiveSteps(string name, int skinType)
+        {
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine($"Hi {name}! based on your skin type, here is your recommended 5 Steps of Skin Care Routine:");
+
+            switch (skinType)
+            {
+                case 1: //oily skin
+                    Console.WriteLine("- STEP 1: Facial Foaming Cleanser");
+                    Console.WriteLine("- STEP 2: Alcohol-Free Toner");
+                    Console.WriteLine("- STEP 3: Niacinamide Serum");
+                    Console.WriteLine("- STEP 4: Oil-free Moisturizer");
+                    Console.WriteLine("- STEP 5: Matte-Finish Sunscreen");
+                    break;
+
+                case 2: //dry skin
+                    Console.WriteLine("- STEP 1: Hydrating Cream Cleanser");
+                    Console.WriteLine("- STEP 2: Hydrating Toner");
+                    Console.WriteLine("- STEP 3: Hyaluronic Acid Serum");
+                    Console.WriteLine("- STEP 4: Ceramide-rich Moisturizer");
+                    Console.WriteLine("- STEP 5: Moisturizing Sunscreen with SPF 30+");
+                    break;
+
+                case 3: //sensitive skn
+                    Console.WriteLine("- STEP 1: Gentle and Fragrance-free Cleanser");
+                    Console.WriteLine("- STEP 2: Alcohol-Free and Soothing Toner");
+                    Console.WriteLine("- STEP 3: Niacinamide or Centella Asiatica Serum");
+                    Console.WriteLine("- STEP 4: Fragrance-free and Ceramide-rich Moisturizer");
+                    Console.WriteLine("- STEP 5: Mineral (zinc oxide or titanium dioxide) Sunscreen");
+                    break;
+
+                case 4: // combi skin (oily and dry)
+                    Console.WriteLine("- STEP 1: Gel-based or Foaming Cleanser");
+                    Console.WriteLine("- STEP 2: Alcohol-Free and Balancing Toner");
+                    Console.WriteLine("- STEP 3: Niacinamide or Hyaluronic Acid Serum");
+                    Console.WriteLine("- STEP 4: Lightweight and Oil-free Gel Moisturizer");
+                    Console.WriteLine("- STEP 5: Broad-spectrum and Lightweight Sunscreen");
+                    break;
+            }
+        }
+
+        static bool AskForAnotherGuide(string name)
+        {
+            Console.WriteLine("Would you like another guide? Please type Yes or No.");
+            string response = Console.ReadLine().ToLower();
+
+            if (response != "yes")
+            {
+                Console.WriteLine($"Thank you, {name}! Keep slaying that glow!");
+                return false;
+            }
+            return true;
         }
     }
 }
