604351876602906065f1ff52e0470831724ce8a0
glaizamaeretardo <retardo.glaizamae18@gmail.com>
2025-05-23 02:14:31 +0800

    added Common layer

---
+++ SCG_BusinessLogic/SCGProcess.cs
@@ -0,0 +1,31 @@
+﻿using SCG_Common;
+using SCG_DataLogic;
+
+namespace SCG_BusinessLogic
+{
+    public static class SCGProcess
+    {
+        public static int FindOutSkinType(bool oilySkin, bool drySkin, bool sensitiveSkin, bool normalSkin)
+        {
+            if (oilySkin && !drySkin && !sensitiveSkin)
+                return (int) SkinType.Oily;
+            if (!oilySkin && drySkin && !sensitiveSkin)
+                return (int) SkinType.Dry;
+            if (sensitiveSkin)
+                return (int) SkinType.Sensitive;
+            if (normalSkin)
+                return (int) SkinType.Normal;
+
+            return (int) SkinType.Combination; //if combination skin
+        }
+        
+        public static string GetSkinTypeName(int skinType)
+        {
+            if(Enum.IsDefined(typeof(SkinType), skinType))
+            {
+                return ((SkinType)skinType).ToString();
+            }
+            return "Unknown";
+        }
+    }
+}
---
+++ SCG_DataLogic/SCGData.cs
@@ -0,0 +1,56 @@
+﻿using SCG_Common;
+
+namespace SCG_DataLogic
+{
+    public class SCGData
+    {
+        private static Dictionary<string, int> savedReference = new();
+
+        public static void SaveUserDetails(string nameToAdd, int skinType) //save user
+        {
+            savedReference[nameToAdd] = skinType;
+        }
+
+        public static Dictionary<string, string> GetAllUserDetails() //view user's details
+        {
+            Dictionary<string, string> userDetails = new();
+            
+            foreach (var user in savedReference)
+            {
+                userDetails[user.Key] = ((SkinType)user.Value).ToString();
+            }
+            return userDetails;
+        }
+
+        public static bool UpdateUserDetails(string userToUpdate, int updatedSkinType, out string newUserSkinType) //update user
+        {
+            newUserSkinType = string.Empty;
+
+            if (savedReference.ContainsKey(userToUpdate))
+            {
+                savedReference[userToUpdate] = updatedSkinType;
+                newUserSkinType = ((SkinType)updatedSkinType).ToString();
+                
+                return true;
+            }
+            return false;
+        }
+
+        public static bool DeleteUserDetails(string nameToDelete) //delete user
+        {
+            return savedReference.Remove(nameToDelete);
+        }
+
+        public static bool SearchUserDetails(string nameToSearch, out string skinType) //search a user
+        {
+            if(savedReference.TryGetValue(nameToSearch, out int st))
+            {
+                skinType = ((SkinType)st).ToString();
+                return true;
+            }
+
+            skinType = string.Empty;
+            return false;
+        }
+    }
+}
